85db3c434239d2e7b25e626d349f1de83a7f0015
youdass101 <hamzechalhoub@gmail.com>
2022-11-05 02:42:52 +0200

    start

---
+++ tuya101/main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("The new Home!")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("first label")
+    label.move(40, 50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
